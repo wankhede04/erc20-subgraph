7b203613cb38f77d43fa447d20a0f42fa2a1d70f
wankhede04 <wankhedevijay12@gmail.com>
2023-10-13 12:27:49 +0530

    Add related config for farming

--- src/farming.ts
+++ src/farming.ts
@@ -1,12 +1,17 @@
-import { Deposit as DepositEvent } from "../generated/DexLpFarming/DexLpFarming";
+import { Deposit as DepositEvent, Withdraw as WithdrawEvent } from "../generated/DexLpFarming/DexLpFarming";
 import { Deposit } from "../generated/schema";
+import { store } from "@graphprotocol/graph-ts";
 
 export function handleDeposit(event: DepositEvent): void {
   let entity = new Deposit(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+    event.params.user.concatI32(event.params.tokenId.toI32())
   );
 
   entity.user = event.params.user;
   entity.tokenId = event.params.tokenId;
   entity.save();
+}
+
+export function handleWithdraw(event: WithdrawEvent): void {
+  store.remove("Deposit", (event.params.user.concatI32(event.params.tokenId.toI32())).toHex());
 }
