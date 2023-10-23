80adf58571980634fcd7fc7dbce52441fb317864
wankhede04 <wankhedevijay12@gmail.com>
2023-10-23 10:41:41 +0530

    Config for 721

--- src/farming.ts
+++ src/farming.ts
@@ -1,24 +1,18 @@
-import { DepositBatch as DepositEvent, WithdrawBatch as WithdrawEvent } from "../generated/DexLpFarming/DexLpFarming";
+import { Deposit as DepositEvent, Withdraw as WithdrawEvent } from "../generated/DexLpFarming/DexLpFarming";
 import { Deposit } from "../generated/schema";
-import { BigInt, store } from "@graphprotocol/graph-ts";
+import { store } from "@graphprotocol/graph-ts";
 
 export function handleDeposit(event: DepositEvent): void {
-  const tokenIds: BigInt[] = event.params.tokenId;
-  for (let index = 0; index < tokenIds.length; index++) {
     let entity = new Deposit(
-      event.params.user.concatI32(event.params.tokenId[index].toI32())
+      event.params.user.concatI32(event.params.tokenId.toI32())
     );
 
     entity.transactionHash = event.transaction.hash;
     entity.user = event.params.user;
     entity.tokenId = event.params.tokenId;
     entity.save();
-  }
 }
 
 export function handleWithdraw(event: WithdrawEvent): void {
-  const tokenIds: BigInt[] = event.params.tokenId;
-  for (let index = 0; index < tokenIds.length; index++) {
-    store.remove("Deposit", (event.params.user.concatI32(event.params.tokenId[index].toI32())).toHex());
-  }
+  store.remove("Deposit", (event.params.user.concatI32(event.params.tokenId.toI32())).toHex());
 }
